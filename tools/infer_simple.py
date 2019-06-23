903fe2a866210cf16f4d473a31f5af71cf1a30f2
Mohit Mendiratta <51849557+mohitm1994@users.noreply.github.com>
2019-06-23 13:07:46 +0200

    Update infer_simple.py
    added print args

--- tools/infer_simple.py
+++ tools/infer_simple.py
@@ -85,7 +85,7 @@ def parse_args():
 def main(args):
     logger = logging.getLogger(__name__)
     merge_cfg_from_file(args.cfg)
-    print(args)
+    print('important',args)
     cfg.NUM_GPUS = 1
     args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
     assert_and_infer_cfg(cache_urls=False)
