eac448f1249b801e1fd99bda309f5b2e7ffc1564
Mohit Mendiratta <51849557+mohitm1994@users.noreply.github.com>
2019-06-25 00:59:27 +0200

    Update project.py
    fgbgbrgbrg

--- tools/project.py
+++ tools/project.py
@@ -3,12 +3,7 @@ import numpy as np
 import cv2
 import trail
 from caffe2.python import workspace
-import sys, os
-script_dir = sys.path[0]
-print(sys.path)
-img_file = open('~/drive/My Drive/Birthday/IMG-20180510-WA0001.jpg')
-print(img_file)
-im = cv2.imread('drive/My Drive/Birthday/IMG-20180510-WA0001.jpg')
+im = cv2.imread('/content/drive/My Drive/Mahabalipuram/20180527_144323.jpg')
 workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
 cfgs = 'configs/DensePose_ResNet101_FPN_s1x-e2e.yaml'
 weights = 'https://dl.fbaipublicfiles.com/densepose/DensePose_ResNet101_FPN_s1x-e2e.pkl'
