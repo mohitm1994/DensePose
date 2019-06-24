070faca90c0566dac4176b0c3b294171515cc053
Mohit Mendiratta <51849557+mohitm1994@users.noreply.github.com>
2019-06-24 22:31:32 +0200

    Update project.py
    uzgjhvh

--- tools/project.py
+++ tools/project.py
@@ -6,6 +6,8 @@ from caffe2.python import workspace
 import sys, os
 script_dir = sys.path[0]
 print(sys.path)
+img_file = open('drive/My Drive/Birthday/IMG-20180510-WA0001.jpg')
+print(img_file)
 im = cv2.imread('drive/My Drive/Birthday/IMG-20180510-WA0001.jpg')
 workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
 cfgs = 'configs/DensePose_ResNet101_FPN_s1x-e2e.yaml'
