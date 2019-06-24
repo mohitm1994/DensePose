4f1fa24380e8ba22da96af8d2f1f03e70a265007
Mohit Mendiratta <51849557+mohitm1994@users.noreply.github.com>
2019-06-24 19:45:09 +0200

    Update project.py
    dghhnszrhsrthsrth

--- tools/project.py
+++ tools/project.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import trail
 from caffe2.python import workspace
-im = cv2.imread('drive/My Drive/Birthday/IMG-20180510-WA0001.jpg')
+im = cv2.imread('DensePoseData/demo_data/xyz.jpg')
 print(im)
 workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
 cfgs = 'configs/DensePose_ResNet101_FPN_s1x-e2e.yaml'
