4fbe431e303412c68490b0978bfb560cd6398eb9
Mohit Mendiratta <51849557+mohitm1994@users.noreply.github.com>
2019-06-24 20:22:49 +0200

    Update project.py
    bdgbybydfbf

--- tools/project.py
+++ tools/project.py
@@ -4,10 +4,9 @@ import cv2
 import trail
 from caffe2.python import workspace
 im = cv2.imread('DensePoseData/demo_data/xyz.jpg')
-print(im)
 workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
 cfgs = 'configs/DensePose_ResNet101_FPN_s1x-e2e.yaml'
 weights = 'https://dl.fbaipublicfiles.com/densepose/DensePose_ResNet101_FPN_s1x-e2e.pkl'
 workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
 i = trail.main(im,cfgs,weights)
-print(i)
+cv2.imwrite('DensePoseData/x_IUV.png',i)
